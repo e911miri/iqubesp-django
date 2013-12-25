442b81ba7dead389db452c033563d61468bb344c
Tomiwa <e911miri@gmail.com>
2013-12-25 22:00:50 +0100

    homepage sample

--- home/views.py
+++ home/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render({}, "home/index.djhtml")
+    return render(request, "home/index.djhtml", {
+        'main': "The Journey of an IT GURU Statts with one skill",
+        "block1": "Learning in sdp is having your mentor right next to you - Kola",
+        "block2": "At SDP, learning has never been more fun - Wande",
+        "block3": "I often ask myself why it is so cheap, 3months - 5k? it's just awesome - Orimolade M."
+    })
