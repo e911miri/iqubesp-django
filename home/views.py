6cbfad5c809476ddee1252591c66b04f094dd034
unknown <tomiwa@tomcat.(none)>
2014-01-07 23:50:06 +0100

    Improvement to the UX of the Student's View

--- home/views.py
+++ home/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from operations.models import Course
 
 # Create your views here.
 def index(request):
-    context = {
-       'main': "The Journey of an IT GURU Statts with one skill",
-        "block1": "Learning in sdp is having your mentor right next to you - Kola",
-        "block2": "At SDP, learning has never been more fun - Wande",
-        "block3": "I often ask myself why it is so cheap, 3months - 5k? it's just awesome - Orimolade M."
-    }
-    template_path = "home/index.djhtml"
+    if request.user.is_authenticated():
+        context = {
+            'courses': Course.objects.all(),
+            'tasks': None,
+        }
+        template_path = "home/student_home.djhtml"
+    else:        
+        context= {
+            'main': "The Journey of an IT GURU Statts with one skill",
+            "block1": "Learning in sdp is having your mentor right next to you - Kola",
+            "block2": "At SDP, learning has never been more fun - Wande",
+            "block3": "I often ask myself why it is so cheap, 3months - 5k? it's just awesome - Orimolade M."
+        }        
+        template_path = "home/index.djhtml"
     return render(request, template_path, context)
 
 def login_view(request):
@@ -28,7 +36,6 @@ def login_view(request):
             return None
     else:
         return render(request, "auth/login.djhtml", {})
-    
-    
+
 def logout_view(request):
     logout(request)
