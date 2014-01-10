45704011d99516bce46fe383acfd15fbfb1ec658
unknown <tomiwa@tomcat.(none)>
2014-01-10 12:34:13 +0100

    Removed redundant urls

--- students/urls.py
+++ students/urls.py
@@ -2,8 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$','students.views.home', name='students_home'),
-    url(r'^login$','django.contrib.auth.views.login', {'template_name': 'auth/login.djhtml'}, name='students_login'),
-    url(r'^logout$','students.views.logout_view', name='students_logout'),
-    url(r'^register/$','students.views.register_view', name='register'),
     
 )
