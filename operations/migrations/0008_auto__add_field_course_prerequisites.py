485b21f1700f601e9d4d8ded5a332e689ba194ee
Tomiwa <e911miri@gmail.com>
2014-01-18 14:38:25 +0100

    :\)

---
+++ operations/migrations/0008_auto__add_field_course_prerequisites.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Course.prerequisites'
+        db.add_column(u'operations_course', 'prerequisites',
+                      self.gf('django.db.models.fields.TextField')(default='A Computer'),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Course.prerequisites'
+        db.delete_column(u'operations_course', 'prerequisites')
+
+
+    models = {
+        u'operations.course': {
+            'Meta': {'object_name': 'Course'},
+            'active': ('django.db.models.fields.BooleanField', [], {}),
+            'category': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'desc': ('django.db.models.fields.TextField', [], {}),
+            'duration': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'link': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'prerequisites': ('django.db.models.fields.TextField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'testimonial': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'operations.studenttestimony': {
+            'Meta': {'object_name': 'StudentTestimony'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'page': ('django.db.models.fields.IntegerField', [], {}),
+            'testimony': ('django.db.models.fields.TextField', [], {})
+        }
+    }
+
+    complete_apps = ['operations']
