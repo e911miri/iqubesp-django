ac417299a1cc718281342fb0c6eedb05f5968607
Tomiwa <e911miri@gmail.com>
2014-01-20 14:57:38 +0100

    heroku deploy

--- operations/admin.py
+++ operations/admin.py
@@ -3,7 +3,13 @@ from operations.models import Course, StudentTestimony
 from students.models import StudentCourse, Payment
 # Register your models here.
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'duration', 'prerequisites', 'active']
+    
+class StudentTestimonyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'testimony', 'page']
+    
+admin.site.register(Course, CourseAdmin)
 admin.site.register(StudentCourse)
 admin.site.register(Payment)
-admin.site.register(StudentTestimony)
+admin.site.register(StudentTestimony, StudentTestimonyAdmin)
